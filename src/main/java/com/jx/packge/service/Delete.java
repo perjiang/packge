20a4904da0b2cb6682a6599f4c79231e3992361e
jiangx <jiangx@qianlong2.net>
2022-07-26 11:26:50 +0800

    add delete.java

---
+++ src/main/java/com/jx/packge/service/Delete.java
@@ -0,0 +1,15 @@
+package com.jx.packge.service;
+
+import java.sql.SQLOutput;
+
+/**
+ * @ClassName Delete
+ * @Description
+ * @Author
+ * @Date 2022/7/26 11:26
+ */
+public class Delete {
+    public static void main(String[] args) {
+        System.out.println("delete");
+    }
+}
