71cb9401ae5600c4833b5eb19584625211f08bae
jiangx <jiangx@qianlong2.net>
2022-07-26 10:53:41 +0800

    1

--- src/main/java/com/jx/packge/TestMonth.java
+++ src/main/java/com/jx/packge/TestMonth.java
@@ -9,18 +9,12 @@ import java.util.Date;
 
 /**
  * @ClassName TestMonth
- * @Description TODO
+ * @Description 1
  * @Author
  * @Date 2022/7/13 11:17
  */
 public class TestMonth {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        LocalDate date = LocalDate.now();
-        LocalDate before = date.minusMonths(-1);
-        System.out.println(before.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth());
-        calendar.setTime(new Date());
-        int max = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-        System.out.println(max);
+        System.out.println("1");
     }
 }
